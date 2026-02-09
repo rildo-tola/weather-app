7df3bd2a14c09255874a6f027720980b11e75e6b
Rildo Tola <tolawonda@gmail.com>
2026-02-09 23:07:12 +0100

    functions and all

--- js/script.js
+++ js/script.js
@@ -1,9 +1,56 @@
+// DOM ELEMENT SELECTION
 const cityInput = document.getElementById("cityInput");
 const searchBtn = document.getElementById("searchBtn");
 
+const weatherContainer = document.getElementById("weatherContainer");
+const cityName = document.getElementById("cityName");
+const temperature = document.getElementById("temperature");
+const description = document.getElementById("description");
+const errorMsg = document.getElementById("errorMsg");
+const loadingMsg = document.getElementById("loadingMsg");
+
+// API KEY
+const API_KEY = "15640c149378b4e587f08bcab5bda9b1";
+
+
+// SEARCH BUTTON EVENT
 searchBtn.addEventListener("click", () => {
   const city = cityInput.value.trim();
   if (city === "") return;
-
-  console.log("Searching weather for:", city);
+  getWeather(city);
 });
+
+// MAIN FUNCTION
+async function getWeather(city) {
+  //RESET UI STATE
+  errorMsg.classList.add("hidden");
+  weatherContainer.classList.add("hidden");
+
+  loadingMsg.classList.remove("hidden");
+  searchBtn.disabled = true;
+
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    );
+
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+
+    const data = await response.json();
+
+    cityName.textContent = data.name;
+    temperature.textContent = `🌡️ ${Math.round(data.main.temp)}°C`;
+    description.textContent = `☁️ ${data.weather[0].description}`;
+
+    weatherContainer.classList.remove("hidden");
+
+  } catch (error) {
+    errorMsg.textContent = "Could not fetch weather. Try another city.";
+    errorMsg.classList.remove("hidden");
+  } finally {
+    loadingMsg.classList.add("hidden");
+    searchBtn.disabled = false;
+  }
+}
